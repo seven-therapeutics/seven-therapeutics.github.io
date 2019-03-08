d3f9ed498435b563e9593e2706c1d8c950f20f98
wlchew <chew.wei.leong@gmail.com>
2019-03-08 23:33:59 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+/*
+author: Boostraptheme
+author URL: https://boostraptheme.com
+License: Creative Commons Attribution 4.0 Unported
+License URL: https://creativecommons.org/licenses/by/4.0/
+*/
+
+
+
+
+
+/*====================================================
+                        TEAM
+====================================================*/
+
+/*====================================================
+                        NAVIGATION
+====================================================*/
+// Show/Hide transparent black navigation
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+
+        } else {
+            // show nav
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+
+(function($) {
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 60
+  });
+})(jQuery); // End of use strict
+
+
+// Smooth scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get/return id like #about, #work, #team and etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 5
+        }, 1250, "easeInOutExpo");
+    });
+});
+
+// Close mobile menu on click
+$(function(){
+
+    $(".navbar-collapse ul li a").on("click touch", function(){
+
+        $(".navbar-toggle").click();
+    });
+});
